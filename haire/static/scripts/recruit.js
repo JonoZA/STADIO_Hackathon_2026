55d4023dbfd229da5d897813ee0ccfbe58f8cb10
Elye <elyefredericks0@gmai.com>
2026-08-21 14:45:03 +0200

    Finished the recruit page and with css and java

--- haire/static/scripts/recruit.js
+++ haire/static/scripts/recruit.js
@@ -11,3 +11,33 @@ infoButtons.forEach(button => {
     });
 
 });
+
+// RADIO BUTTONS
+
+const radioButtons = document.querySelectorAll(
+    'input[type="radio"][name="application"]'
+);
+
+radioButtons.forEach(radio => {
+
+    radio.addEventListener("click", function() {
+
+        if (this.dataset.wasChecked === "true") {
+
+            this.checked = false;
+
+            this.dataset.wasChecked = "false";
+
+        } else {
+
+            radioButtons.forEach(otherRadio => {
+                otherRadio.dataset.wasChecked = "false";
+            });
+
+            this.dataset.wasChecked = "true";
+
+        }
+
+    });
+
+});
