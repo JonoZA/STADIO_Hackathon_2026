1b3546e32a27f18b029093f42ef53c811a3dbfa3
Freddy <elyefredericks0@gmail.com>
2026-08-21 12:59:40 +0200

    Created recruit page

--- haire/app.py
+++ haire/app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route("/recruit")
+def recruit():
+    return render_template("recruit.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
